efe1b56da01ff4050defa4f41b929c49b4bfb8bf
osohoob <osohoob275@gmail.com>
2024-11-28 16:50:15 +0800

    project starting

---
+++ src/components/Home/LandingPage.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
